c9fd56791c8fbeadf92c0f5433dc74271fecf91c
tarentomaheshvakkund <139739142+tarentomaheshvakkund@users.noreply.github.com>
2025-05-14 12:33:32 +0530

    Removed all the extra attributes from application.properties
    - These properties are specific to cb-ext-userprofile-service .

--- src/main/java/com/igot/cb/util/CbServerProperties.java
+++ src/main/java/com/igot/cb/util/CbServerProperties.java
@@ -13,17 +13,6 @@ import java.util.List;
 @Setter
 public class CbServerProperties {
 
-  @Value("${default.content.properties}")
-  private String defaultContentProperties;
-
-  @Value("${content-service-host}")
-  private String contentHost;
-
-  @Value("${content-read-endpoint}")
-  private String contentReadEndPoint;
-
-  @Value("${content-read-endpoint-fields}")
-  private String contentReadEndPointFields;
 
   @Value("${redis.insights.index}")
   private int redisInsightIndex;
@@ -33,37 +22,6 @@ public class CbServerProperties {
 
   @Value("${sb.api.key}")
   private String sbApiKey;
-
-  @Value("${learner.service.url}")
-  private String learnerServiceUrl;
-
-  @Value("${sb.org.search.path}")
-  private String orgSearchPath;
-
-  @Value("${notify.service.host}")
-  private String notifyServiceHost;
-
-  @Value("${notify.service.path.async}")
-  private String notificationAsyncPath;
-
-
-  @Value("${notification.support.mail}")
-  private String supportEmail;
-
-  @Value("${learner.service.url}")
-  private String sbUrl;
-
-  @Value("${sunbird.user.search.endpoint}")
-  private String userSearchEndPoint;
-
-  @Value("${lms.user.read.path}")
-  private String userReadEndPoint;
-
-  @Value("${sb.search.service.host}")
-  private String sbSearchServiceHost;
-
-  @Value("${sb.composite.v4.search}")
-  private String sbCompositeV4Search;
 
   @Value("${http.client.request.factory.timeout}")
   private int requestTimeoutMs;
