f8cfbafd40df0bc4d3d9ab09cf28363e7b5768f1
tarentomaheshvakkund <139739142+tarentomaheshvakkund@users.noreply.github.com>
2025-05-21 13:24:21 +0530

    KB-9792 |  BE| DEV | API to provide the list of Institutions and degrees.
    1. API added to fetch the state and district details using the extended user read.

--- src/main/java/com/igot/cb/profile/controller/ProfileController.java
+++ src/main/java/com/igot/cb/profile/controller/ProfileController.java
@@ -43,6 +43,12 @@ public class ProfileController {
         return new ResponseEntity<>(response, HttpStatus.valueOf(response.getResponseCode().value()));
     }
 
+    @GetMapping("/extended/locationDetails/{userId}")
+    public ResponseEntity<Object> getLocationDetails(@PathVariable("userId") String userId, @RequestHeader(value = Constants.X_AUTH_TOKEN, required = true) String authToken) {
+        ApiResponse response = profileService.readFullExtendedProfile(userId,"locationDetails",authToken);
+        return new ResponseEntity<>(response, HttpStatus.valueOf(response.getResponseCode().value()));
+    }
+
     @GetMapping("/extended/achievements/{userId}")
     public ResponseEntity<Object> getAchievements(@PathVariable("userId") String userId, @RequestHeader(value = Constants.X_AUTH_TOKEN, required = true) String authToken) {
         ApiResponse response = profileService.readFullExtendedProfile(userId,"achievements",authToken);
