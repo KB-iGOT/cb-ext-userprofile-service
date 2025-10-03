9a4db771dd7d6bedb41b0e843d437a16726a629a
sreerag-tarento <sreerag.sajeesh@tarento.com>
2025-10-03 13:57:25 +0530

    KAR-1076 - Discrepancy in the certificate count in user insights and profile page (#80)

--- src/main/java/com/igot/cb/util/Constants.java
+++ src/main/java/com/igot/cb/util/Constants.java
@@ -455,7 +455,7 @@ public class Constants {
     public static final String PROFILE_PREFERENCE = "profilePreference";
     public static final String AUTH_TOKEN = "Authorization";
     public static final String APPROVED = "Approved";
-
+    public static final String USER_EXTERNAL_COURSE_ENROLMENTS = "user_external_enrolments";
     private Constants() {
     }
 }
