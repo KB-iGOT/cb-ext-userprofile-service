126afee2cc416137667874d971c05168d525a9b3
sahilchaudhary <sahil.chaudhary@tarento.com>
2026-01-13 09:58:41 +0530

    resolving the critical issue for sonarQube

--- src/main/java/com/igot/cb/util/EncryptionService.java
+++ src/main/java/com/igot/cb/util/EncryptionService.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 public interface EncryptionService {
 
-    String ALGORITHM = "AES";
+    String ALGORITHM = "AES/GCM/NoPadding";
     int ITERATIONS = 3;
     byte[] keyValue =
             new byte[] {'T', 'h', 'i', 's', 'A', 's', 'I', 'S', 'e', 'r', 'c', 'e', 'K', 't', 'e', 'y'};
