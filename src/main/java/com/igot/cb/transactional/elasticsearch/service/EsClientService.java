b6c8dbf83788d0097cba27b3bbe85d6083c5791f
pathiknandi <pathik.nandi@tarento.com>
2026-02-03 12:32:50 +0530

    KB-13011 this is the ElasticSearch client implementation.

---
+++ src/main/java/com/igot/cb/transactional/elasticsearch/service/EsClientService.java
@@ -0,0 +1,29 @@
+package com.igot.cb.transactional.elasticsearch.service;
+
+import co.elastic.clients.elasticsearch._types.query_dsl.Query;
+import co.elastic.clients.elasticsearch.core.BulkResponse;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.igot.cb.transactional.elasticsearch.dto.SearchCriteria;
+import com.igot.cb.transactional.elasticsearch.dto.SearchResult;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+
+public interface EsClientService {
+  String addDocument(String esIndexName, String type, String id, Map<String, Object> document, String JsonFilePath);
+
+  String updateDocument(String index, String indexType, String entityId, Map<String, Object> document, String JsonFilePath);
+
+  void deleteDocument(String documentId, String esIndexName);
+
+  void deleteDocumentsByCriteria(String esIndexName, Query query);
+
+  SearchResult searchDocuments(String esIndexName, SearchCriteria searchCriteria) throws Exception;
+
+  boolean isIndexPresent(String indexName);
+
+  BulkResponse saveAll(String esIndexName, List<JsonNode> entities) throws IOException;
+
+}
